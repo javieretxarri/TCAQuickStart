ce582cbd227300545b02b96224ef20d03c8a5fbe
Javier Etxarri <javieretxarrimarin@gmail.com>
2023-06-14 13:53:05 +0200

    Initial Commit

---
+++ TCAQuickStart/TCAQuickStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAQuickStartApp.swift
+//  TCAQuickStart
+//
+//  Created by Javier Etxarri on 14/6/23.
+//
+
+import SwiftUI
+
+@main
+struct TCAQuickStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
